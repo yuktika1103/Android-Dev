4a8077abc2d2284355f1e3c5d4f5ef8c51f9e516
Akshay-181 <akshayjoshi.x@gmail.com>
2025-02-23 08:59:08 +0000

    Initial Commit

--- Ass-1_Q1/app/src/main/java/com/example/ass1_q1/MainActivity.java
+++ Ass-1_Q1/app/src/main/java/com/example/ass1_q1/MainActivity.java
@@ -1,68 +1,16 @@
 package com.example.ass1_q1;
 
 import android.os.Bundle;
-import android.view.View;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView resultTextView;
-    private String currentInput = "";
-    private String operator = "";
-    private double firstNumber = 0;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        resultTextView = findViewById(R.id.resultTextView);
-    }
 
-    public void onDigitClick(View view) {
-        Button button = (Button) view;
-        currentInput += button.getText().toString();
-        resultTextView.setText(currentInput);
-    }
-
-    public void onOperatorClick(View view) {
-        Button button = (Button) view;
-        if (!currentInput.isEmpty()) {
-            firstNumber = Double.parseDouble(currentInput);
-            operator = button.getText().toString();
-            currentInput = "";
-        }
-    }
-
-    public void onEqualClick(View view) {
-        if (!currentInput.isEmpty()) {
-            double secondNumber = Double.parseDouble(currentInput);
-            double result = 0;
-
-            switch (operator) {
-                case "+": result = firstNumber + secondNumber; break;
-                case "-": result = firstNumber - secondNumber; break;
-                case "*": result = firstNumber * secondNumber; break;
-                case "/": 
-                    if (secondNumber != 0) {
-                        result = firstNumber / secondNumber;
-                    } else {
-                        resultTextView.setText("Error");
-                        return;
-                    }
-                    break;
-            }
-
-            resultTextView.setText(String.valueOf(result));
-            currentInput = "";
-            operator = "";
-        }
-    }
-
-    public void onClearClick(View view) {
-        currentInput = "";
-        operator = "";
-        firstNumber = 0;
-        resultTextView.setText("0");
+        // Display Good Morning Message using Toast
+        Toast.makeText(this, "Good Morning!", Toast.LENGTH_SHORT).show();
     }
 }
